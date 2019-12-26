399f0dced1d4cbf1003b7f012039d3aa81629c3c
Tivo Yudha <tivo@jojonomic.com>
2019-12-26 20:08:09 +0700

    fixed implementation

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,12 +3,17 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+    
+    var flutterViewController: FlutterViewController!
+    var flutterMethodChannel: FlutterMethodChannel!
+    
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    let flutterViewController: FlutterViewController = self.window.rootViewController as! FlutterViewController
-    let flutterMethodChannel: FlutterMethodChannel = FlutterMethodChannel(name: "com.tivoyudha.method_channel_app.channel", binaryMessenger: flutterViewController.binaryMessenger)
+    flutterViewController = self.window.rootViewController as? FlutterViewController
+    flutterMethodChannel = FlutterMethodChannel(name: "com.tivoyudha.method_channel_app.channel", binaryMessenger: flutterViewController.binaryMessenger)
+    
     flutterMethodChannel.setMethodCallHandler { (methodCall, result) in
         if (methodCall.method.elementsEqual("showNativeView")) {
             let viewController: NextViewController = NextViewController(nibName: "NextViewController", bundle: nil)
@@ -22,4 +27,8 @@ import Flutter
     
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    func printSomething() {
+        print("Something")
+    }
 }
--- lib/src/SplashScreen.dart
+++ lib/src/SplashScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:method_channel_app/src/HomeScreen.dart';
 import 'package:method_channel_app/src/bloc/SplashScreenBloc.dart';
 
@@ -11,8 +12,16 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  static const platform = const MethodChannel('com.tivoyudha.method_channel_app.channel');
   
   SplashScreenBloc _splashScreenBloc = new SplashScreenBloc();
+
+  @override
+  void initState() {
+    super.initState();
+    platform.setMethodCallHandler(_handleMethod);
+  }
   
   @override
   Widget build(BuildContext context) {
@@ -21,9 +30,19 @@ class _SplashScreenState extends State<SplashScreen> {
     });
     
     return Scaffold(
+      key: _scaffoldKey,
       body: Center(
         child: Image.asset('assets/image/ic_flutter.png', height: 200, width: 200),
       ),
     );
   }
+
+  Future<dynamic> _handleMethod(MethodCall methodCall) async {
+    switch(methodCall.method) {
+      case "sendMessage":
+        _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text(methodCall.arguments)));
+    }
+  }
 }
+
+final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
